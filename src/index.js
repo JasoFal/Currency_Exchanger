15a37b4cd762dc604f3be9a97babdf020acc768d
Jason Falk <jasonfalk9@gmail.com>
2023-09-14 09:34:57 -0700

    Add function for api call.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import CurrencyExchangerApi from './js/CurrencyExchangerApi';
+
+async function getExchangeRates(currencyCode, amountInUSD) {
+  const response = await CurrencyExchangerApi.newCurrencyExchangeApiCall(currencyCode, amountInUSD);
+  if (response["result"] == "success") {
+    console.log(response);
+    printElements(response, currencyCode);
+  } else {
+    printError(response, currencyCode);
+  }
+}
+
+function printError(error, currency) {
+  document.querySelector("#show-response").innerText = `There was an error accessing the currency data for ${currency}:
+  ${error}`
+}
+
+// function printElements(data) {
+//   document.querySelector('#showResponse').innerText = ``
+// }
+
