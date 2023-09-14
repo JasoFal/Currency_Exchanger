51593de882eaf311525f73efb5314c70c2746e0a
Jason Falk <jasonfalk9@gmail.com>
2023-09-14 14:02:17 -0700

    Fix api call errors.

--- src/js/CurrencyExchangerApi.js
+++ src/js/CurrencyExchangerApi.js
@@ -1,8 +1,7 @@
 export default class CurrencyExchangerApi {
   static async newCurrencyExchangeApiCall(currencyCode, baseCurrencyAmount) {
     try {
-      const response = await fetch(`GET https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/USD/${currencyCode}/${baseCurrencyAmount}
-      `);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencyCode}/${baseCurrencyAmount}`);
       const jsonifiedNewCurrencyExchangeResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}`;
