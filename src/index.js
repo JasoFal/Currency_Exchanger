7d91cbc9e47a0afef68c055fcda929d0a1161d70
Jason Falk <jasonfalk9@gmail.com>
2023-09-14 14:01:55 -0700

    Add eventListener for submit event. Typo fixes.

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import CurrencyExchangerApi from './js/CurrencyExchangerApi';
 async function getExchangeRates(currencyCode, amountInUSD) {
   const response = await CurrencyExchangerApi.newCurrencyExchangeApiCall(currencyCode, amountInUSD);
   if (response["result"] == "success") {
-    console.log(currencyCode);
-    console.log(response);
     printElements(response, currencyCode);
   } else {
     printError(response, currencyCode);
@@ -16,16 +14,22 @@ async function getExchangeRates(currencyCode, amountInUSD) {
 
 function printError(error, currency) {
   document.querySelector("#show-response").innerText = `There was an error accessing the currency data for ${currency}:
-  ${error}`
+  ${error}`;
 }
 
 function printElements(data) {
-  document.querySelector("#showResponse").innerText = `The USD to ${data[1]} conversion is ${data[0].conversion_result}.`
+  document.querySelector("#show-response").innerText = `The ${data.base_code} to ${data.target_code} conversion is ${data.conversion_result} at a rate of ${data.conversion_rate}.`;
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
   const currencyCode = document.querySelector("#type-of-currency").value;
   const amountInUSD = document.querySelector("#usd-amount").value;
+  document.querySelector("#type-of-currency").value = null;
+  document.querySelector("#usd-amount").value = null;
   getExchangeRates(currencyCode, amountInUSD);
 }
+
+window.addEventListener("load", function () {
+  document.querySelector("form").addEventListener("submit", handleFormSubmission);
+});
