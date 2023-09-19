c2a0bab462e4f24193c4908f3e79a6ce65d97877
Jason Falk <jasonfalk9@gmail.com>
2023-09-19 15:27:08 -0700

    Add more error handling to api call.

--- src/js/CurrencyExchangerApi.js
+++ src/js/CurrencyExchangerApi.js
@@ -4,8 +4,13 @@ export default class CurrencyExchangerApi {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencyCode}/${baseCurrencyAmount}`);
       const jsonifiedNewCurrencyExchangeResponse = await response.json();
       if (!response.ok) {
-        const errorMessage = `${response.status} ${response.statusText}`;
-        throw new Error(errorMessage);
+        if (jsonifiedNewCurrencyExchangeResponse.result === "error") {
+          const inputFailErrorMessage = `${jsonifiedNewCurrencyExchangeResponse.result} ${jsonifiedNewCurrencyExchangeResponse["error-type"]}`;
+          throw new Error(inputFailErrorMessage);
+        } else {
+          const apiFailErrorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(apiFailErrorMessage);
+        }
       }
       return jsonifiedNewCurrencyExchangeResponse;
     } catch(error) {
